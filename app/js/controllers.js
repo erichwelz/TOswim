c235ed73a4b94b2dc246d91d60d13736eedba797
Erich Welz <erichwelz@gmail.com>
2015-11-20 08:59:07 -0500

    http service to pull in phones JSON, pretty print pool list, refactor tests with mocked backend calls

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,18 +4,10 @@
 
 var TOswimApp = angular.module('TOswimApp', []);
 
-TOswimApp.controller('PoolListCtrl', function ($scope) {
-  $scope.pools = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.',
-     'age': 1},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age': 2},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age': 3}
-  ];
+TOswimApp.controller('PoolListCtrl', ['$scope', '$http', function ($scope, $http) {
+  $http.get('../scraper/phones.json').success(function(data) {
+    $scope.pools = data;
+    });
 
   $scope.orderProp = 'age'; //sets default for orderProp scope
-});
+}]);
--- test/unit/controllerSpec.js
+++ test/unit/controllerSpec.js
@@ -3,18 +3,33 @@
 describe('TOSwim controllers', function() {
 
   describe ('PoolListCtrl', function() {
-    var scope, ctrl;
+    var scope, ctrl, $httpBackend;
 
+    // Load our app module before each test
     beforeEach(module('TOswimApp'));
 
-    beforeEach(inject(function($controller) {
-      scope = {};
+    // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_)
+    // This allows us to inject a srice but then attach it to a variable
+    // with the same names as the service in order to avoid a name conflict.
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('../scraper/phones.json').
+        respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+
+      scope = $rootScope.$new();
       ctrl = $controller('PoolListCtrl', {$scope:scope});
     }));
 
-    it('should create "pools" model with 3 pools', function() {
-        expect(scope.pools.length).toBe(3);
+
+
+    it('should create "pools" model with 2 pools fetched from xhr', function() {
+      expect(scope.pools).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.pools).toEqual([{name: 'Nexus S'},
+                                   {name: 'Motorola DROID'}]);
     });
+
 
     it('should set the default value of orderProp model', function() {
       expect(scope.orderProp).toBe('age');
