d19f0c59b2676ddd7bd88d4021555fc4742f33cd
Erich Welz <erichwelz@gmail.com>
2015-11-27 15:33:03 -0500

    Start of custom filter implementation, almost useful

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,5 +9,29 @@ TOswimApp.controller('PoolListCtrl', ['$scope', '$http', function ($scope, $http
     $scope.pools = data;
     });
 
+  $scope.filter = {};
+
+  $scope.getTimes = function () {
+    return ($scope.pools || []).map(function (pool) {
+      return pool.times;
+    }).filter(function ( pool, idx, arr) {
+      return arr.indexOf(pool) == idx;
+    });
+  };
+
+  $scope.filterByTime = function ( pool) {
+    return $scope.filter[pool.times] || noFilter($scope.filter);
+  };
+
+
+  function noFilter(filterObj) {
+    for (var key in filterObj) {
+        if (filterObj[key]) {
+            return false;
+        }
+    }
+    return true;
+  }
+
   $scope.orderProp = 'address'; //sets default for orderProp scope
 }]);
