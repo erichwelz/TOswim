f025d3768bb00db2bd04034c49629428de237bed
Erich Welz <erichwelz@gmail.com>
2015-11-28 17:21:20 -0500

    return pool distance in KM

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -50,6 +50,24 @@ TOswimApp.controller('PoolListCtrl', ['$scope', '$http', function ($scope, $http
     return dates;
   }
 
+  $scope.getDistance = function distance(lat1, lon1, lat2, lon2) {
+  if (isNaN(lat2) === true) {
+    lat2 = 43.6792740; //my latitude
+  }
+
+  if (isNaN(lon2) === true) {
+    lon2 = -79.3592080; //my longitude
+  }
+
+  var p = 0.017453292519943295;    // Math.PI / 180
+  var c = Math.cos;
+  var a = 0.5 - c((lat2 - lat1) * p)/2 +
+          c(lat1 * p) * c(lat2 * p) *
+          (1 - c((lon2 - lon1) * p))/2;
+
+  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
   function noFilter(filterObj) {
     for (var key in filterObj) {
         if (filterObj[key]) {
