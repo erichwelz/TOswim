2990dd4a1d363c65706177dd8a63afe466cb20b2
Erich Welz <erichwelz@gmail.com>
2015-12-03 11:08:44 -0500

    refactor getDistance to use objects

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -33,6 +33,15 @@ TOswimApp.controller('PoolListCtrl', ['$scope', '$http', function ($scope, $http
     return Object.keys(pool.times).indexOf(selected_date) > -1 || noFilter($scope.filter);
   };
 
+  function noFilter(filterObj) {
+    for (var key in filterObj) {
+        if (filterObj[key]) {
+            return false;
+        }
+    }
+    return true;
+  }
+
   function dateMaker(days) {
     // returns array of dates in format of "Fri Nov 27" starting with today
     if (isNaN(days) === true) {
@@ -50,35 +59,26 @@ TOswimApp.controller('PoolListCtrl', ['$scope', '$http', function ($scope, $http
     return dates;
   }
 
-  $scope.distanceSort = function ( pool ) {
-    return $scope.getDistance(pool.coordinates.latitude, pool.coordinates.longitude);
+  $scope.distanceSort = function (pool) {
+    return $scope.getDistance(pool.coordinates);
   };
 
+  $scope.getDistance = function (poolLoc) {
+    var currentLoc = getCurrentLocation();
 
-  $scope.getDistance = function (lat1, lon1, lat2, lon2) {
-  if (isNaN(lat2) === true) {
-    lat2 = 43.6792740; //my latitude
-  }
+    var p = 0.017453292519943295;    // Math.PI / 180
+    var c = Math.cos;
+    var a = 0.5 - c((currentLoc.latitude - poolLoc.latitude) * p)/2 +
+            c(poolLoc.latitude * p) * c(currentLoc.latitude * p) *
+            (1 - c((currentLoc.longitude - poolLoc.longitude) * p))/2;
 
-  if (isNaN(lon2) === true) {
-    lon2 = -79.3592080; //my longitude
-  }
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+  };
 
-  var p = 0.017453292519943295;    // Math.PI / 180
-  var c = Math.cos;
-  var a = 0.5 - c((lat2 - lat1) * p)/2 +
-          c(lat1 * p) * c(lat2 * p) *
-          (1 - c((lon2 - lon1) * p))/2;
-
-  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
-};
-
-  function noFilter(filterObj) {
-    for (var key in filterObj) {
-        if (filterObj[key]) {
-            return false;
-        }
-    }
-    return true;
+  function getCurrentLocation() {
+    var currentLoc = {};
+    currentLoc.latitude = 43.6792740; //my latitude
+    currentLoc.longitude = -79.3592080; //my longitude
+    return currentLoc;
   }
 }]);
